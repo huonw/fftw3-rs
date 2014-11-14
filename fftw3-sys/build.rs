e8847a08b4c5003df6b982c1842370455cae42e2
Huon Wilson <dbau.pp+github@gmail.com>
2014-11-14 21:34:28 +1100

    Clarify error message.

--- fftw3-sys/build.rs
+++ fftw3-sys/build.rs
@@ -5,6 +5,6 @@ fn main() {
         Err(..) => {}
     }
 
-    println!("failed to find libfftw3");
+    println!("failed to find libfftw3, and automatically building it is currently unimplemented.");
     std::os::set_exit_status(1);
 }
