19195b11dec685c5b73469581cd232b65bdff203
bobtwinkles <srkoser+github@gmail.com>
2016-02-24 15:13:14 -0500

    Allow library to build on 1.6.0 stable
      - Remove dependency on std::sync::StaticMutex by replacing it with a
        std::sync::Mutex initialized by lazy_static.
      - Replace use of unstable std::raw::Slice with std::slice::from_raw_parts
      - Replace use of depreciated Path.as_os_str().to_cstring()

--- src/lock.rs
+++ src/lock.rs
@@ -2,14 +2,17 @@
 //! that only one thread is executing these at a time. This module
 //! provides a lock for this purpose.
 
-use std::sync::{StaticMutex, MUTEX_INIT};
+use std::sync::{Mutex};
 
-/// Hold this lock when doing anything thread-unsafe with FFTW.
-pub static LOCK: StaticMutex = MUTEX_INIT;
+// pub static LOCK: StaticMutex = MUTEX_INIT;
+lazy_static! {
+    /// Hold this lock when doing anything thread-unsafe with FFTW.
+    pub static ref FFTW_MUTEX: Mutex<()> = Mutex::new(());
+}
 
 /// Run the given closure inside the critical section of the FFTW
 /// lock.
 pub fn run<A, F: FnOnce() -> A>(f: F) -> A {
-    let _g = LOCK.lock();
+    let _g = FFTW_MUTEX.lock();
     f()
 }
